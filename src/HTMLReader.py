a92d4ce6cec44e97f23bbf34b5b666c15aed68e6
futer8 <email.for.repository@gmail.com>
2017-02-26 01:09:29 +0000

    date acquisition

--- src/HTMLReader.py
+++ src/HTMLReader.py
@@ -2,13 +2,15 @@ from bs4 import BeautifulSoup as bs
 from contextlib import closing
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
-
-SEARCH_KEYWORD = 'table'
+import re
+import datetime
 
 class HTMLReader:
 
     def __init__(self, config_path):
-        self._config = {'Search keyword': ''}
+        self._config = {'Search keyword': '', 'Date separator': ''}
+        self._html = bs('', 'html.parser')
+        self._html_text = ''
         try:
             fh = open(config_path, 'r')
             lines = fh.readlines()
@@ -19,7 +21,7 @@ class HTMLReader:
         error_msg = 'Incorrect HTMLReader config file ' + config_path + '! It should be (without <>):'
         for key in self._config.keys():
             error_msg += ' \n ' + key + ': <value>'
-
+        
         config_update_count = 0
         try:
             for line in lines:
@@ -44,9 +46,30 @@ class HTMLReader:
         try:
             driver = webdriver.PhantomJS()
             driver.get(url)
-            WebDriverWait(driver, timeout = timeout).until(lambda x: x.find_element_by_tag_name(SEARCH_KEYWORD))
+            WebDriverWait(driver, timeout = timeout).until(lambda x: x.find_element_by_tag_name(self._config['Search keyword']))
             page_source = driver.page_source
             driver.quit()
-            self.html = bs(page_source, 'html.parser')
+            self._html = bs(page_source, 'html.parser')
+            self._html_text = page_source
         except:
             raise RuntimeError('Error reading the supplied URL ' + url)
+
+    def GetDate(self, regex_pattern_file):
+        try:
+            fh = open(regex_pattern_file, 'r')
+            pattern = fh.read().strip()
+            fh.close()
+        except FileNotFoundError:
+            raise FileNotFoundError('Date stamp regex pattern file ' + regex_pattern_file + ' not found!')
+        try:
+            data_date = re.search(pattern, self._html_text)
+            data_date = data_date.group()
+            data_date = data_date.split(self._config['Date separator'])
+            data_date = datetime.date(int(data_date[2]), int(data_date[1]), int(data_date[0]))
+        except AttributeError:
+            raise AttributeError('Date pattern not found in the text!')
+
+        if data_date.year >= 2017: # This program has been written in 2017. (Semi) live feeds should be from at least 2017.
+            return data_date
+        else:
+            raise ValueError('Problem with date - year should be at least 2017!')
--- tests/test_HTMLReader.py
+++ tests/test_HTMLReader.py
@@ -1,9 +1,10 @@
 import unittest
-from src.HTMLReader import *
+from src.HTMLReader import HTMLReader
 
 from bs4 import BeautifulSoup as bs
 import os
 import pathlib
+import datetime
 
 RES_PATH = 'tests/res/'
 
@@ -23,14 +24,16 @@ class Test(unittest.TestCase):
         with self.assertRaises(RuntimeError) as context:
             instance = HTMLReader(RES_PATH + file_name)
         error_msg = 'Incorrect HTMLReader config file ' + RES_PATH + file_name + '! It should be (without <>):'
-        error_msg += ' \n Search keyword: <value>'
-        self.assertTrue(error_msg == str(context.exception))
+        self.assertTrue(error_msg in str(context.exception))
+        self.assertTrue('Search keyword: <value>' in str(context.exception))
+        self.assertTrue('Date separator: <value>' in str(context.exception))
 
     def test_GetHTML_success(self):
         absolute_path = os.path.abspath(RES_PATH + 'GPW.html')
         uri = pathlib.Path(absolute_path).as_uri()
         self.instance.GetHTML(uri, 0)
-        self.assertEqual(type(self.instance.html), type(bs('','html.parser')))
+        self.assertEqual(type(self.instance._html), type(bs('','html.parser')))
+        self.assertEqual(self.instance._html, bs(self.instance._html_text, 'html.parser'))
         
     def test_GetHTML_failure(self):
         incorrect_url = 'non-existent URL'
@@ -38,8 +41,37 @@ class Test(unittest.TestCase):
             self.instance.GetHTML(incorrect_url, 0)
         self.assertTrue('Error reading the supplied URL ' + incorrect_url == str(context.exception))
 
-#    def test_CheckTimeStamp(self):
-#        result = self.instance.CheckTimeStamp('
+    def test_GetDate_success(self):
+        absolute_path = os.path.abspath(RES_PATH + 'GPW.html')
+        uri = pathlib.Path(absolute_path).as_uri()
+        self.instance.GetHTML(uri, 0)
 
+        result = self.instance.GetDate(RES_PATH + 'Date_stamp_regex_pattern.txt')
+        self.assertEqual(type(datetime.date.today()), type(result))
+        self.assertGreaterEqual(result.year, 2017)
+
+    def test_GetDate_failure_reading(self):
+        incorrect_file_name = 'non-existent path'
+        with self.assertRaises(FileNotFoundError) as context:
+            result = self.instance.GetDate(incorrect_file_name)
+        self.assertTrue('Date stamp regex pattern file ' + incorrect_file_name + ' not found!' == str(context.exception))
+
+    def test_GetDate_failure_incorrect_date(self):
+        absolute_path = os.path.abspath(RES_PATH + 'GPW_incorrect_date.html')
+        uri = pathlib.Path(absolute_path).as_uri()
+        self.instance.GetHTML(uri, 0)
+
+        with self.assertRaises(ValueError) as context:
+            result = self.instance.GetDate(RES_PATH + 'Date_stamp_regex_pattern.txt')
+        self.assertTrue('Problem with date - year should be at least 2017!' == str(context.exception))
+
+    def test_GetDate_failure_pattern_not_found(self):
+        with self.assertRaises(AttributeError) as context:
+            result = self.instance.GetDate(RES_PATH + 'Date_stamp_regex_pattern.txt')
+        self.assertTrue('Date pattern not found in the text!' == str(context.exception))
+
+
+
+        
 if __name__ == '__main__':
     unittest.main()
